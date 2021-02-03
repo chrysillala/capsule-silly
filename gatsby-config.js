ef1920286cac7f49593a08022423a0662276cddd
chrysilla mayasari <chrysilla_mysr@hotmail.com>
2021-02-03 16:36:48 +0700

    remove manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,20 +27,20 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Capsule Hotel`,
-        short_name: `CapsuleHotel`,
-        start_url: `/`,
-        background_color: `#F0FFF0`,
-        theme_color: `#E6E6FA`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Capsule Hotel`,
+    //     short_name: `CapsuleHotel`,
+    //     start_url: `/`,
+    //     background_color: `#F0FFF0`,
+    //     theme_color: `#E6E6FA`,
+    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    //     display: `standalone`,
+    //     icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // The offline plugin should be listed after the manifest plugin
     // so that the offline plugin can cache the created manifest.webmanifest.
     `gatsby-plugin-offline`,
