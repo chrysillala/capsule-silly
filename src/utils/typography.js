28e46f3530dc8060237266c80b16ac5944ff6f76
chrysilla mayasari <chrysilla_mysr@hotmail.com>
2021-02-03 14:13:05 +0700

    Revert "init commit"
    This reverts commit 4593d2842900f38d91c99aa63f3fd1292dabf79f.

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography"
-import typoTheme from "typography-theme-noriega"
-
-const typography = new Typography(typoTheme)
-
-export default typography
-export const rhythm = typography.rhythm
